bb3732545a93e6caf172b27208a464f952f38f9f
Infinyty <maciej.ptasinski@gmail.com>
2022-06-01 18:23:51 +0300

    test2

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ const app = express()
 const port = process.env.PORT || 3131
 const screenshot = require('./screenshot')
 
-app.get('/', (req, res) => res.status(200).json({ status: 'ok' }))
+app.get('/status', (req, res) => res.status(200).json({ status: 'ok' }))
+app.get('/', function (req, res, next) {
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from("<form action='/screenshot'><input name=query></form>"))
+  });
 
 app.get('/screenshot', (req, res) => {
-  const url = req.query.url
-  ;(async () => {
-    const buffer = await screenshot(url)
-    res.setHeader('Content-Disposition', 'attachment; filename="screenshot.png"')
-    res.setHeader('Content-Type', 'image/png')
-    res.send(buffer)
-  })()
-})
+    const url = req.query.url
+    ;(async () => {
+      const buffer = await screenshot(url)
+      res.setHeader('Content-Disposition', 'attachment; filename="screenshot.png"')
+      res.setHeader('Content-Type', 'image/png')
+      res.send(buffer)
+    })()
+  })
+
+app.get('/test', (req, res) => {
+    
+    ;(async () => {
+      const buffer = await screenshot('http://google.com/')
+      res.setHeader('Content-Disposition', 'attachment; filename="screenshot.png"')
+      res.setHeader('Content-Type', 'image/png')
+      res.send(buffer)
+    })()
+  })
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
